55a3418be5d184843823cce000aaafa35656af80
Ian Roland <vertosomnium@gmail.com>
2016-09-21 05:11:13 +0200

    i changed the string variables to match my twitter account info

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'y1lsk0aNg03sMmMvDOJsElhIN',
+  consumer_secret:      'SQif0uGi34rVlp4elnhCbNU0q59uZ9scySxmqVyNchdBkzCRqH',
+  access_token:         '778425835594346496-7vSnMPTNDZU2spEDJNf73HzO7hieryD',
+  access_token_secret:  'iaKiiqnHGWZFyT97Pbjvxg61a4n25lq4yuDtoX697hpbJ'
 }
