9840673092f74f4f75a85a0e4f90d207a6b98d44
Violet Forest <violetforest@Violet-Forest.local>
2016-09-22 17:25:07 -0500

    destroyTweets()

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
-var politician = {slug: "hillary-donald", owner_screen_name: "papipaulina", count: 200, include_rts: false};
+var politician = {slug: "hillary-donald", owner_screen_name: "papipaulina", count: 20, include_rts: false};
 
 function retweetLatest() {
 	T.get('lists/statuses', politician, function (error, data) {
@@ -30,8 +30,38 @@ function retweetLatest() {
 	});
 }
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
+var paulina = {screen_name: "papipaulina", count: 200};
+
+function destroyTweets() {
+	T.get('statuses/user_timeline', paulina, function (error, data) {
+	  console.log(error, data);
+
+		for(var i = 0; i < data.length; i++){
+		  if (!error) {
+
+		  	// ...then we grab the ID of the tweet we want to retweet...
+			var retweetId = data[i].id_str;
+
+			// ...and then we tell Twitter we want to retweet it!
+			T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+				if (response) {
+					console.log('Success! Check your bot, it should have retweeted something.')
+				}
+				if (error) {
+					console.log('There was an error with Twitter:', error);
+				}
+			})
+		  }
+		  // However, if our original search request had an error, we want to print it out here.
+		  else {
+		  	console.log('There was an error with your hashtag search:', error);
+		  }
+		}
+	});
+}
+
+// retweetLatest();
+destroyTweets();
+
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
