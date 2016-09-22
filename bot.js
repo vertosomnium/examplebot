ace63eb49390a309838ae01efb918f8000604f9f
Violet Forest <violetforest@Violet-Forest.local>
2016-09-22 10:36:24 -0500

    manually tweets most recent cybertwee tweet on  cybertwee's timeline

--- bot.js
+++ bot.js
@@ -1,27 +1,21 @@
-// Our Twitter library
 var Twit = require('twit');
-
-// We need to include our configuration file
 var T = new Twit(require('./config.js'));
+var politician = {screen_name: "cybertwee", count: 1};
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var politician = {screen_name: "cybertwee", count: 300};
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
 	T.get('statuses/user_timeline', politician, function (error, data) {
-	  // log out any errors and responses
 	  console.log(error, data);
-	  // If our search request to the server had no errors...
+
 	  if (!error) {
+
 	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
+		// var retweetId = data.id_str[0];
+
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+		T.post('statuses/retweet/:id', {id: '777888357250572292' }, function (error, response) {
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
 			}
-			// If there was an error with our Twitter call, we print it out here.
 			if (error) {
 				console.log('There was an error with Twitter:', error);
 			}
