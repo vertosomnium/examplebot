8395d9cefb944a9d616161d0facbb2f92d0153b5
Violet Forest <violetforest@Violet-Forest.local>
2016-09-22 17:08:26 -0500

    i did it

--- bot.js
+++ bot.js
@@ -1,36 +1,32 @@
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
-var politician = {screen_name: "cybertwee", count: 20};
+var politician = {slug: "hillary-donald", owner_screen_name: "papipaulina", count: 20, include_rts: false};
 
 function retweetLatest() {
-	T.get('statuses/user_timeline', politician, function (error, data) {
+	T.get('lists/statuses', politician, function (error, data) {
 	  console.log(error, data);
 
-    // var tweets = data[0];
-    // for (var i = 0; i < tweets.length; i++) {
-    //     console.log(tweets[i].text);
-    // }
-		for(var i = 0;  < data.length; i++){
-			  if (!error) {
+		for(var i = 0; i < data.length; i++){
+		  if (!error) {
 
-	  // ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data[i].id_str;
+		  	// ...then we grab the ID of the tweet we want to retweet...
+			var retweetId = data[i].id_str;
 
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	}
+			// ...and then we tell Twitter we want to retweet it!
+			T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+				if (response) {
+					console.log('Success! Check your bot, it should have retweeted something.')
+				}
+				if (error) {
+					console.log('There was an error with Twitter:', error);
+				}
+			})
+		  }
+		  // However, if our original search request had an error, we want to print it out here.
+		  else {
+		  	console.log('There was an error with your hashtag search:', error);
+		  }
+		}
 	});
 }
 
