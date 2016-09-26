f06a86af3a5665ae73d2227afdca23a3e5ff6980
Violet Forest <violetforest@Violet-Forest.local>
2016-09-26 08:09:16 -0500

    refreshes every 6 seconds, need ur help incrementing a and b each time?

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
+var a = 768153606765412352;
+var b = 764153606765412352;
 var paulina = {
 	screen_name: "papipaulina",
 	count: 5000
@@ -8,7 +10,9 @@ var politician = {
 	slug: "hillary-donald",
 	owner_screen_name: "papipaulina",
 	count: 5000,
-	include_rts: false
+	include_rts: false,
+	since_id: b,
+	max_id: a
 };
 
 function sleep(milli) {
@@ -19,16 +23,14 @@ function sleep(milli) {
 
 function retweetLatest() {
 	T.get('lists/statuses', politician, function (error, data) {
-	  // console.log(error, data);
+	  console.log(error, data);
 
 		for (var i = (data.length - 1); i > 0; i--) {
 		  if (!error) {
 
-			var retweetId = data[i].id_str;
+			var retweetId = data[i].id;
 			var retweetDate = data[i].created_at;
-			// console.log(data[i].created_at + ': ' + retweetId);
 			console.log(i + ': ' + retweetDate + ': ' + retweetId);
-
 //			T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
 //				if (response) {
 //					 console.log('Success! Check your bot, it should have retweeted something.')
@@ -43,13 +45,15 @@ function retweetLatest() {
 		  else {
 		  	console.log('There was an error with your hashtag search:', error);
 		  }
+		  a += 4000000000000000;
+		  b += 4000000000000000;
 		}
 	});
 }
 
 function destroyTweets() {
 	T.get('statuses/user_timeline', paulina, function (error, data) {
-	  console.log(error, data);
+	  // console.log(error, data);
 
 		for(var i = (data.length-1); i > 0; i--){
 		  if (!error) {
@@ -76,4 +80,4 @@ retweetLatest();
 // destroyTweets();
 
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+setInterval(retweetLatest, 1000 * 6);
