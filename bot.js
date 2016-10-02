91d1880d344dba184dac3aae269706ec43c9459d
Violet Forest <violetforest@Violet-Forest.local>
2016-10-02 18:24:48 -0500

    thank you andrew monks

--- bot.js
+++ bot.js
@@ -1,57 +1,56 @@
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
+var max = 756000000005000001;
+
 var paulina = {
-	screen_name: "papipaulina",
+	screen_name: "HeSheSaid2016",
 	count: 180
 };
 
+var getIdTrump = {
+	screen_name: "realdonaldtrump",
+	count: 1,
+	max_id: 746000000005000001
+}
 function sleep(milli) {
   	var currentTime = new Date().getTime();
 		while (currentTime + milli >= new Date().getTime()) {
   }
 }
 
-function retweetLatest() {
+function retweetLatest(user, max_id=765000000005000001) {
 	var politician = {
-		slug: "hillary-donald",
-		owner_screen_name: "papipaulina",
+		screen_name: user,
 		count: 180,
-		include_rts: false,
-		since_id: since,
-		max_id: max
+		max_id: max_id
 	};
-	T.get('lists/statuses', politician, function (error, data) {
+
+	console.log("getting some tweets", politician)
+
+
+	T.get('statuses/user_timeline', politician, function (error, data) {
+		if (error) return console.log("ERROR", error)
+
 	  // console.log(error, data);
 
-		for (var i = (data.length - 1); i > 0; i--) {
-		  if (!error) {
-
-			var retweetId = data[i].id_str;
-			var retweetDate = data[i].created_at;
-			var lastTweet = data[0].id;
-			console.log(i + ': ' + retweetDate + ': ' + retweetId);
-			T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-				if (response) {
-					 console.log('retweetId:' + retweetId)
-					 sleep(2000);
-				}
-				if (error) {
-					 console.log('There was an error with Twitter:', error);
-				}
-			})
-		  }
-		  // However, if our original search request had an error, we want to print it out here.
-		  else {
-		  	console.log('There was an error with your hashtag search:', error);
-		  }
-		}
+		var lastTweet = data[0].id;
 		since = lastTweet;
-		max = since + 1000000000000000;
+
+		timelines[user] = timelines[user].concat(data);
+
+		if (max < 8000000000000000) {
+			max += 1000000000000000;
+			setTimeout(retweetLatest, 2000, user, max);
+		}
+		else {
+			doneness[user] = true;
+		}
+
 	});
 }
 
 function destroyTweets() {
-	T.get('statuses/user_timeline', paulina, function (error, data) {
+	T.get('statuses/user_timeline', politician, function (error, data) {
 	  // console.log(error, data);
 
 		for(var i = (data.length-1); i > 0; i--){
@@ -75,9 +74,60 @@ function destroyTweets() {
 	});
 }
 
-retweetLatest();
-// destroyTweets();
+var doneness = {
+	realDonaldTrump: false,
+	HillaryClinton: false
+}
+
+var timelines = {
+	realDonaldTrump: [],
+	HillaryClinton: []
+}
+
+retweetLatest("realDonaldTrump");
+retweetLatest("HillaryClinton");
+
+function checkDonenessandMaybeRetweetIfReady() {
+	console.log("checking doneness")
+	if (doneness["realDonaldTrump"] && doneness["HillaryClinton"]) {
+		console.log("both feeds are done")
+
+		var combinedTimelines = [].concat(timelines["realDonaldTrump"]).concat(timelines["HillaryClinton"]);
+
+		var sortedTimelines = combinedTimelines.sort(function (a, b) {
+			if (a.id_str > b.id_str) {
+				return 1;
+			}
+			if (a.id_str < b.id_str) {
+				return -1;
+			}
+			return 0;
+		});
+
+		console.log(sortedTimelines.map(tweet => `${tweet.user.name} on ${tweet.created_at}`))
+
+		sortedTimelines.forEach(function (tweet, index) {
+			setTimeout(function () {
+				T.post('statuses/retweet/' + tweet.id_str, { }, function (error, response) {
+					if (error) {
+						console.log('There was an error with Twitter:', error);
+					}
+					if (response) {
+						console.log('retweetId:' + tweet.id_str)
+					}
+				})
+			}, 5100 * index)
+		})
+	}
+	else {
+		setTimeout(checkDonenessandMaybeRetweetIfReady, 5000);
+	}
+}
+
+// checkDonenessandMaybeRetweetIfReady();
+destroyTweets();
+// getId();
 
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 
-setInterval(retweetLatest, 1000 * 60 * 17); //retweet every 17 min
+// setInterval(retweetLatest, 1000 * 60 * 17); //retweet every 17 min
